a49c7f6cc13c83b0ecfb1ef575d0f3711729a60f
Neel Agarwal <neelkagarwal@gmail.com>
2025-04-08 20:30:01 -0500

    LOG: - Added prv config method to fix the paths based on env setting - Created method for grabbing the yaml from github-linguist's linguist repo and caching it to prevent extra calls (pretty lightweight might keep it as integral backup) - Thinking of going against recommended advice of decoupling datasource from jobs and putting it in extraction, unless extraction becomes cluttered with GitWrap objects then yes - Moving code out of init's for readability of file purpose - Linguist cache file

--- Config.py
+++ Config.py
@@ -11,7 +11,8 @@ class Config:
         if cls._instance is None:
             cls._instance = super(Config, cls).__new__(cls)
             cls._instance._load_env_vars()
-            cls._instance._engine_meta()
+            cls._instance._load_engine()
+            cls._instance._get_github_linguist()
         return cls._instance
     
     def _load_env_vars(self):
@@ -19,18 +20,24 @@ class Config:
         self.GIT_TOKEN = os.environ.get('GIT_TOKEN')
         self.AZURE_CONN = os.environ.get('AZURE_CONN')
         self.STORAGE_ACCT_CONN = os.environ.get('STORAGE_ACCT_CONN')
-        self.STORAGE = Path(os.environ.get('STORAGE', '/resources'))
+        self.STORAGE = Path(os.environ.get('STORAGE', 'resources'))
         self.LOG_LEVEL = self.__get_log_level()
         self.LOG_FILE = os.environ.get('LOG_FILE', 'app.log')
         self.SLEEP = os.environ.get('SLEEP', 1)
+        self.__correct_paths()
         return self
     
-    def _engine_meta(self):
+    def _load_engine(self):
         self.__get_db_name()
         self.ENGINE_PATH = self.STORAGE / self.db_name
         self.ENGINE_URI = f'sqlite:///{self.ENGINE_PATH}'
         return self
-    
+
+    def _get_github_linguist(self):
+        self.LINGUIST_URL = 'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml'
+        self.LINGUIST_CACHE = self.STORAGE / 'linguist_cache.yaml'
+
+
     def __get_db_name(self):
         if self.ENV == 'production':
             self.db_name = 'gitwrap.sqlite'
@@ -44,7 +51,14 @@ class Config:
             if os.environ.get('APP_ENV') == 'development' 
             else 'INFO'
         )
-
+    
+    def __correct_paths(self):
+        if self.ENV == 'production':
+            pass
+        else:
+            repo_root = Path(__file__).resolve().parent
+            self.STORAGE = repo_root / self.STORAGE
+        return self
 
 # EOF
 
